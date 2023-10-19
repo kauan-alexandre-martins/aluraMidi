60aa2fb1705f693a47a54f02a739d6f9568cb2dd
kauan-alexandre-martins <125566988+kauan-alexandre-martins@users.noreply.github.com>
2023-10-19 10:59:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+alert("para continuar forneça seus dados bancarios!!!")
+
+function tocasompom() {
+    document.querySelector('#som_tecla_pom').play();}
+    document.querySelector('.tecla_pom').onclick=tocasompom
+
+document.querySelector('.tecla_pom')
+document.querySelector('.tecla_clap')
+document.querySelector('.tecla_tim')
+document.querySelector('.tecla_puff')
+document.querySelector('.tecla_splash')
+document.querySelector('.tecla_toim')
+document.querySelector('.tecla_psh')
+document.querySelector('.tecla_tic')
+document.querySelector('.tecla_tom')
+

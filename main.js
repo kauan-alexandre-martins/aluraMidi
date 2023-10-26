19e033a530e78044d7ffac01d6c7ddfe1abf96c6
kauan-alexandre-martins <125566988+kauan-alexandre-martins@users.noreply.github.com>
2023-10-26 10:58:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 alert("para continuar forneça seus dados bancarios!!!")
 
-function tocasompom() {
+function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();}
-    document.querySelector('.tecla_pom').onclick=tocasompom
+    document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.tecla_pom')
-document.querySelector('.tecla_clap')
-document.querySelector('.tecla_tim')
-document.querySelector('.tecla_puff')
-document.querySelector('.tecla_splash')
-document.querySelector('.tecla_toim')
-document.querySelector('.tecla_psh')
-document.querySelector('.tecla_tic')
-document.querySelector('.tecla_tom')
+    function tocaSomclap() {
+        document.querySelector('#som_tecla_clap').play();}
+        document.querySelector('.tecla_clap').onclick = tocaSomclap;
 
+        function alert1() {
+            document.querySelector('#som_tecla_tim').play();}
+            document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
+
+        function alert2() {
+            document.querySelector('#som_tecla_puff').play();}
+            document.querySelector('.tecla_puff').onclick = tocaSompuff;
+
+
+        function alert3() {
+            document.querySelector('#som_tecla_splash').play();}
+            document.querySelector('.tecla_splash').onclick = tocaSomsplash;
+
+
+        function alert4() {
+            document.querySelector('#som_tecla_toim').play();}
+            document.querySelector('.tecla_toim').onclick = tocaSomtoim;
+
+
+        function alert5() {
+            document.querySelector('#som_tecla_psh').play();}
+            document.querySelector('.tecla_psh').onclick = tocaSompsh;
+
+
+        function alert6() {
+            document.querySelector('#som_tecla_tic').play();}
+            document.querySelector('.tecla_tic').onclick = tocaSomtic;
+
+
+        function alert7() {
+            document.querySelector('#som_tecla_tom').play();}
+            document.querySelector('.tecla_tom').onclick = tocaSomtom;
+
+const listadeteclas =document.querySelectorAll('.tecla')
